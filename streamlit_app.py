5597d4b497aecb21745fe8c679f20bca5b9562a9
Alessio Di Lorenzo <a.dilorenzo@izs.intra>
2024-03-04 10:37:47 +0100

    Aggiunti indicatori statistiche di base

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 from io import StringIO
 import requests
+from datetime import datetime, timedelta
 
 # Scarica il contenuto del Google Sheet
 url = "https://docs.google.com/spreadsheets/d/15zspXv1dM__F0uunaG9hT_PsrPfU5MZJ-0IzjULzDXI/export?format=csv"
@@ -26,6 +27,18 @@ df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y')
 df['Media settimanale (Kg)'].fillna(0, inplace=True)
 
 df.sort_values(by="Data", inplace=True, ascending=True)
+
+# Calcola il valore medio dell'orario di pesata
+df['Ora'] = pd.to_datetime(df['Ora'], format='%H:%M').dt.time
+
+# Converti l'orario in minuti
+df['Ora_in_minuti'] = df['Ora'].apply(lambda x: x.hour * 60 + x.minute)
+
+# Calcola la media dei minuti
+media_minuti = df['Ora_in_minuti'].mean()
+# Converti il risultato in formato datetime.time
+media_ora = datetime.min + timedelta(minutes=media_minuti)
+media_ora = media_ora.time()
 
 # Crea il grafico con Plotly
 fig = go.Figure()
@@ -94,7 +107,68 @@ st.set_page_config(
 )
 
 st.title(':scales: Weight report :weight_lifter:')
-st.markdown("L'applicazione è alimentata attraverso un *Google Spreadsheet* e mostra l'andamento del **peso giornaliero** e della **media settimanale**. Le pesate vengono effettuate nelle medesime condizioni (dopo colazione, in mutande) e all'incirca allo stesso orario, tra le 6:00 e le 9:00 del mattino")
+# st.markdown("Statistiche peso aggiornate quotidianamente. I valori usati per le metriche vengono prelevati da un *Google Spreadisheet*")
+st.divider()
+col1, col2, col3 = st.columns(3)
+
+st.write(
+    """
+    <style>
+    [data-testid="stMetricDelta"] svg {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+st.markdown("""
+    <style>
+    div[data-testid="metric-container"] {
+    background-color: rgba(28, 131, 225, 0.1);
+    border: 1px solid rgba(28, 131, 225, 0.1);
+    padding: 5% 5% 5% 10%;
+    border-radius: 5px;
+    color: rgb(30, 103, 119);
+    overflow-wrap: break-word;
+    }
+
+    /* breakline for metric text         */
+    div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {
+    overflow-wrap: break-word;
+    white-space: break-spaces;
+    color: red;
+    }
+    </style>
+    """
+, unsafe_allow_html=True)
+
+
+with col1:
+    # Trova il valore minimo dell'orario
+    min_ora = divmod(df['Ora_in_minuti'].min(), 60)
+    # Trova il valore massimo dell'orario
+    max_ora = divmod(df['Ora_in_minuti'].max(), 60)
+    st.metric(
+        ":clock7: Orario medio pesata", 
+        media_ora.strftime('%H:%M'), 
+        f"min: {min_ora[0]:02g}:{min_ora[1]:02g}, max: {max_ora[0]:02g}:{max_ora[1]:02g}"
+    )
+    
+with col2:
+    # Trova l'indice del valore minimo del peso giornaliero
+    indice_minimo_peso = df['Kg'].idxmin()
+    # Estrai la data corrispondente all'indice minimo
+    data_minimo_peso = df.loc[indice_minimo_peso, 'Data']
+    giorno_minimo_peso = df.loc[indice_minimo_peso, 'Giorno']
+    st.metric(":arrow_down: Minimo", df['Kg'].min(), giorno_minimo_peso+", "+str(data_minimo_peso.strftime("%d/%m/%Y")))
+    
+with col3:
+    # Trova l'indice del valore massimo del peso giornaliero
+    indice_massimo_peso = df['Kg'].idxmax()
+    # Estrai la data corrispondente all'indice minimo
+    data_massimo_peso = df.loc[indice_massimo_peso, 'Data']
+    giorno_massimo_peso = df.loc[indice_massimo_peso, 'Giorno']
+    st.metric(":arrow_up: Massimo", df['Kg'].max(), giorno_massimo_peso+", "+str(data_massimo_peso.strftime("%d/%m/%Y")))
 
 tab1, tab2 = st.tabs(["Grafico", "Tabella"])
 
@@ -102,6 +176,7 @@ with tab1:
     st.plotly_chart(fig, use_container_width=True)
     
 with tab2:
+    df.drop('Ora_in_minuti', inplace=True, axis='columns')
     df['Media settimanale (Kg)'] = df['Media settimanale (Kg)'].replace(0,'')
     df['Kcal Piano nutrizionale'] = df['Kcal Piano nutrizionale'].fillna('')
     
